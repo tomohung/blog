690266ad1d605383b8b25544999e8e9d53abbadf
Tomo Hung <tomohung@gmail.com>
2016-08-02 22:19:03 +0800

    5.6 fix new.html after setup model article

--- web/controllers/article_controller.ex
+++ web/controllers/article_controller.ex
@@ -1,8 +1,10 @@
 defmodule Blog.ArticleController do
   use Blog.Web, :controller
+  alias Blog.Article
 
   def new(conn, _params) do
-    render conn, "new.html"
+    changeset = Article.changeset(%Article{})
+    render conn, "new.html", changeset: changeset
   end
 
   def create(conn, params) do
